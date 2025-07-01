3a484b64b5aaf2e0ef48f5e90074ab1ad43e401f
Taufikrw <124210031@student.upnyk.ac.id>
2025-07-01 15:01:13 +0700

    login fe

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,29 +3,28 @@
 @section('title', 'Login')
 
 @section('content')
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-        <div class="w-full max-w-md">
-            <div class="bg-white shadow-md rounded-lg p-6">
-                <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
+    <div class="flex min-h-screen">
+        <div class="w-7xl h-screen pl-20 bg-[#1B277C]">
+            <div class="bg-[#0B1870] h-screen justify-center items-center flex flex-col shadow-xl/25">
+                <h2 class="text-5xl text-white font-extrabold text-left w-full px-28 mb-12">Login</h2>
 
-                <form method="POST" action="{{ route('login.store') }}">
+                <form method="POST" action="{{ route('login.store') }}" class="w-full px-28">
                     @csrf
 
                     <div class="mb-4">
-                        <label for="username" class="block text-gray-700">Username</label>
                         <input id="username" type="text"
-                            class="mt-1 p-3 block w-full border-gray-300 rounded-md shadow-sm @error('username') border-red-500 @enderror"
-                            name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                            class="block w-full py-3 border-b border-[#C8C6C6] placeholder-[#C8C6C6] focus:outline-none focus:border-white text-white bg-transparent @error('username') border-red-500 @enderror"
+                            name="username" value="{{ old('username') }}" required autocomplete="username"
+                            placeholder="Username">
                         @error('username')
                             <span class="text-red-500 text-sm mt-1">{{ $message }}</span>
                         @enderror
                     </div>
 
-                    <div class="mb-6">
-                        <label for="password" class="block text-gray-700">Password</label>
+                    <div class="mb-12">
                         <input id="password" type="password"
-                            class="mt-1 p-3 block w-full border-gray-300 rounded-md shadow-sm @error('password') border-red-500 @enderror"
-                            name="password" required autocomplete="current-password">
+                            class="block w-full py-3 border-b border-[#C8C6C6] placeholder-[#C8C6C6] focus:outline-none focus:border-white text-white bg-transparent @error('password') border-red-500 @enderror"
+                            name="password" required autocomplete="current-password" placeholder="Password">
                         @error('password')
                             <span class="text-red-500 text-sm mt-1">{{ $message }}</span>
                         @enderror
@@ -33,11 +32,18 @@
 
                     <div class="flex items-center justify-between">
                         <button type="submit"
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            class="w-full bg-[#FFAF10] hover:bg-[#ED982E] text-white font-bold py-2 px-4 rounded-4xl focus:outline-none cursor-pointer">
                             Login
                         </button>
                     </div>
                 </form>
+            </div>
+        </div>
+        <div class="w-full h-screen pr-20 bg-[#4868FF]">
+            <div class="bg-[#4868FF] h-screen justify-center items-center flex flex-col shadow-[10px_0px_10px_0px_rgba(0,0,0,0.1)]">
+                <h2 class="text-5xl text-white font-extrabold mb-3 text-center">Asisten Laboratorium</h2>
+                <h2 class="text-5xl text-white font-extrabold mb-3 text-center">Jurusan Informatika</h2>
+                <img src="{{ asset('assets/images/pair-aslab-transparant.svg') }}" alt="">
             </div>
         </div>
     </div>
